bc6f0892c11045242d3cef5444f250b9fd163b07
xx <1804113857@qq.com>
2023-07-24 14:28:17 +0800

    第二次上传到github

---
+++ file01.java
@@ -0,0 +1,13 @@
+public class file01{
+	public static void main(String []args){
+		int x = 5;
+		int y = 7;
+		System.out.println(add(x,y));
+	}
+}
+
+class int add (int x,int y){
+	int sum = 0;
+	sum = x + y;
+	return sum;
+}
